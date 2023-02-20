19f2bff87f3fd30be87003c80605ef03beadb823
Kamil Monicz <kamil@monicz.pl>
2023-02-20 13:44:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# A script that — just works!
+
+This script will fix the image and video metadata of the Google Photos Takeout, so that it can be used with other software.
+
+No javascript or any other bs.
+
+## Dependencies
+
+- bash _(yes, I know)_
+- exiftool
+- jq
+
+## Usage
+
+1. Download and extract your [Google Photos Takeout](https://takeout.google.com/)
+2. Download the script and put it inside the `Takeout` folder
+3. Run it
+
+![Directory guide screenshot](./directory-screenshot.png)
+
+## [LICENSE](https://github.com/Zaczero/google-photos-takeout.sh/blob/main/LICENSE)
---
+++ google-photos-takeout.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# configure shellscript
+set -e
+shopt -s globstar
+
+# ensure dependencies are installed
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq is not installed. Aborting."; exit 1; }
+command -v exiftool >/dev/null 2>&1 || { echo >&2 "exiftool is not installed. Aborting."; exit 1; }
+
+# change directory to the script directory
+cd "$(dirname "$0")"
+
+# array to store $fullpath for batch processing
+fullpaths=()
+
+# find all JSON files and enumerate over them
+files=(./**/*.json)
+
+for i in "${!files[@]}"; do
+  file="${files[$i]}"
+
+  # read the filename from the JSON file
+  filename=$(jq -r .title "$file")
+
+  # skip if filename is empty
+  if [ -z "$filename" ]; then
+	  continue
+  fi
+
+  # read the timestamp from the JSON file
+  timestamp=$(jq -r .photoTakenTime.timestamp "$file")
+
+  # get the directory of the JSON file
+  directory=$(dirname "$file")
+
+  # iterate over the original and edited file
+  for f in "$filename" "${filename%.*}-edited.${filename##*.}"; do
+
+    # get the full path of the file
+    fullpath="$directory/$f"
+
+    # skip if file does not exist
+    if [ ! -f "$fullpath" ]; then
+      continue
+    fi
+
+    # print progress
+    echo "[$((i+1))/${#files[@]}] $file : $fullpath"
+
+    # hardlink (it's fast) JSON to $fullpath.json
+    if [ ! -f "$fullpath.json" ]; then
+      ln "$file" "$fullpath.json"
+    fi
+
+    # set modification time from unix timestamp
+    touch -m -t "$(date -d @"$timestamp" +%Y%m%d%H%M.%S)" "$fullpath"
+
+    # append to array
+    fullpaths+=("$fullpath")
+
+  done
+done
+
+# deduplicate fullpaths (sort does not matter but is nice)
+readarray -t fullpaths < <(printf "%s\n" "${fullpaths[@]}" | sort -u)
+
+# print fullpaths size
+echo "Found ${#fullpaths[@]} unique files to process"
+
+# ask for confirmation
+read -p "Looks correct? [y/N] " -n 1 -r
+printf "\n"
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  exit 1
+fi
+
+# run exiftool on all files
+exiftool \
+  -api LargeFileSupport=1 \
+  -d %s \
+  -tagsfromfile "%d%f.%e.json" \
+  "-GPSAltitude<GeoDataAltitude" \
+  "-GPSLatitude<GeoDataLatitude" \
+  "-GPSLatitudeRef<GeoDataLatitude" \
+  "-GPSLongitude<GeoDataLongitude" \
+  "-GPSLongitudeRef<GeoDataLongitude" \
+  "-DateTimeOriginal<PhotoTakenTimeTimestamp" \
+  -overwrite_original \
+  -preserve \
+  -progress \
+  "${fullpaths[@]}" || true
+
+# optional cleanup JSON files
+read -p "Delete all JSON files? [y/N] " -n 1 -r
+printf "\n"
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  rm --verbose ./**/*.json
+fi
